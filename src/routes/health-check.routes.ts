5e51365c82751799dfbfd2eba96a6f70f133c17e
Alla Oleksyn <allaoleksyn@gmail.com>
2023-10-17 00:05:54 +0300

    chore: clean up code

--- src/routes/health-check.routes.ts
+++ src/routes/health-check.routes.ts
@@ -41,38 +41,4 @@ healthCkeck.get('/', (_req: Request, res: Response) => {
   res.status(200).json(healthCheck);
 });
 
-/**
- * @swagger
- * /health-check/{readiness}:
- *  get:
- *     summary: Readiness check
- *     tags: [HealthCheck]
- *     description: Readiness checks ensure that the application instance is ready to receive requests
- *     parameters:
- *      - name: readiness
- *        in: path
- *        description: Readiness checks
- *        required: true
- *        type: string
- *        example: ready / not-ready
- *     responses:
- *       200:
- *         $ref: '#/components/responses/200'
- *       404:
- *          $ref: '#/components/responses/404'
- *       500:
- *          $ref: '#/components/responses/500'
- *       503:
- *          $ref: '#/components/responses/503'
- */
-healthCkeck.route('/:readiness').get(async (req: Request, res: Response) => {
-  console.log('Readiness Check Request');
-
-  if (req.params.readiness === 'ready') {
-    res.status(200).json({ status: 'Service ailable' });
-  } else {
-    res.status(503).json({ status: 'Service Unavailable' });
-  }
-});
-
 export default healthCkeck;
